03eb3bd3a55b6a7302f4e4cdc35c917e52f48c85
tiwarekarsanika <tiwarekarsanika@gmail.com>
2025-02-07 20:39:39 +0530

    favicon, home, minor changes

--- serene-stays-website/src/components/Navbar/Navbar.jsx
+++ serene-stays-website/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import DrawerMenu from "./DrawerMenu";
 import { Link } from "react-router-dom";
-import Virar from "../../pages/VillaPages/Virar";
 import logo from "../../assets/SSlogo_page-0001.jpg";
 
 function Navbar() {
@@ -9,7 +8,7 @@ function Navbar() {
         <div className="flex flex-row justify-between items-center px-4 py-2 shadow-xl" style={{ backgroundColor: '#0a1a19', boxShadow: '0 0.5rem 2rem rgba(244,178,154, 0.1)' }}>
             <div className="flex flex-row items-center space-x-6 ml-6">
                 <img src={logo} alt="Serene Stays Logo" className="w-12 h-12 rounded-full" />
-                <h1 className="text-4xl">Serene Stays</h1>
+                <h1 className="text-4xl cursor-pointer"><Link to="/">Serene Stays</Link></h1>
             </div>
             
             <div className="flex flex-row items-center space-x-10 mr-6">
